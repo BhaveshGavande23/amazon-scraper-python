6a3c7c9dfafb231c6bc575ea49603495236b8d30
BhaveshGavande23 <195341843+BhaveshGavande23@users.noreply.github.com>
2025-11-17 17:15:14 +0530

    Add calculator functions for display, calculate, and clear

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function displayvalue(val) {
+    document.getElementById("display").value += val;
+}
+
+function calculate() {
+    let expression = document.getElementById("display").value;
+    let result = eval(expression);  // calculate result
+    document.getElementById("display").value = result;
+}
+
+function clearScreen() {
+    document.getElementById("display").value = "";
+}
 
